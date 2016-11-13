82824cb2171f511ad8e95ea98d64d1ba3f15437a
Abdulrhman Mohamed <a.mohamed@alumni.ubc.ca>
2016-11-13 15:19:20 -0800

    Refctored printing functions to TreePrinterClass

---
+++ tree_printer.cpp
@@ -0,0 +1,9 @@
+//
+//  tree_printer.cpp
+//  BST
+//
+//  Created by Abdulrhman Mohamed on 2016-11-13.
+//  Copyright © 2016 Abdulrhman Mohamed. All rights reserved.
+//
+
+#include "tree_printer.hpp"
---
+++ tree_printer.hpp
@@ -0,0 +1,14 @@
+//
+//  tree_printer.hpp
+//  BST
+//
+//  Created by Abdulrhman Mohamed on 2016-11-13.
+//  Copyright © 2016 Abdulrhman Mohamed. All rights reserved.
+//
+
+#ifndef tree_printer_hpp
+#define tree_printer_hpp
+
+#include <stdio.h>
+
+#endif /* tree_printer_hpp */
