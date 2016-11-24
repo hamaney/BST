69522d5e15cc902fdbbc9953d4354441b6336f5d
Abdulrhman Mohamed <a.mohamed@alumni.ubc.ca>
2016-11-24 09:39:50 -0800

    implimented tree balancer functions
    implimented --
                            |-- isLeftHeavy
                            |-- isRightHeavy
                            |-- is LL baranch

--- tree_balancer.cpp
+++ tree_balancer.cpp
@@ -7,16 +7,28 @@
 //
 
 #include "tree_balancer.hpp"
-
+using namespace BSTNS::TreeBalancer::PrivateHelper;
 namespace BSTNS {
 namespace TreeBalancer {
-bool BalanceTheTree(NodeUPtr& root);
-namespace PrivateHelper {
-enum BalancingFactor { LL, LR, RR, RL };
 
-bool isLeftHeavy_(NodeUPtr& root);
-bool isRighttHeavy_(NodeUPtr& root);
-bool isBalanced_(NodeUPtr& root);
+// bool BalanceTheTree(NodeUPtr& node) {}
+
+namespace PrivateHelper {
+
+bool isLeftHeavy_(const Node* const node) {
+  return BalanceFactorOfNode_(node) >= 2;
+}
+bool isRightHeavy_(const Node* const node) {
+  return BalanceFactorOfNode_(node) <= -2;
+}
+bool isBalanced_(const Node* const node) {
+  return ((BalanceFactorOfNode_(node) < 2) and
+          (BalanceFactorOfNode_(node) > -2));
+}
+bool isPerfectlyBalanced_(const Node* const node) {
+  return (BalanceFactorOfNode_(node) == 0);
+}
+
 int BalanceFactorOfNode_(const Node* const node) {
   if (!node) {
     assert(0);
@@ -35,6 +47,24 @@ int BalanceFactorOfNode_(const Node* const node) {
     return left_child_balance_factor - right_child_balance_factor;
   }
 }
+bool isLeftHeavyWithLeftHeavyChild_(const Node* const node) {
+  /*if(isLeftHeavy_(node)){
+    return isLeftHeavy_(node->left.get());
+  }else {
+    return false;
+    }
+    */
+  // return (isLeftHeavy_(node))? (BalanceFactorOfNode_(node->left.get())>=1):
+  // false ;
+  return (isLeftHeavy_(node)) ? (BalanceFactorOfNode_(node->left.get()) == 1)
+                              : false;
+}
+bool isLeftHeavyWithRightHeavyChild_(const Node* const node) {
+  return (isRightHeavy_(node)) ? (BalanceFactorOfNode_(node->right.get()) == -1)
+                              : false;
+}
+// bool isRightHeavyWithRightHeavyChild_(const Node* const node) {}
+// bool isRightHeavyWithLeftHeavyChild_(const Node* const node) {}
 }  // End PrivateHelper::
 }  // End TreeBalancer::
-}  // End BSTNS
+}  // End BSTNS::
--- tree_balancer.hpp
+++ tree_balancer.hpp
@@ -9,8 +9,8 @@
 #ifndef tree_balancer_hpp
 #define tree_balancer_hpp
 #include <cassert>
-#include "node.hpp"
 #include <iostream>
+#include "node.hpp"
 /*
 namespace BSTNS{
     namespace TreeBalancer {
@@ -19,41 +19,45 @@ namespace BSTNS{
 /*
  Cases
 
-  []
+      []
+     /L
+   []    -> leftR
   /L
- [] -> leftR
- /L
 []
 
- []
- /L
-[] -> left then right rotation
- \R
- []
-
-[]
- \R
- [] -> right r
+   []
+  /L
+[]       -> left then right rotation
   \R
-  []
+   []
 
 []
- \R
- [] - >right then left rotation
- /L
+  \R
+   []    -> right r
+     \R
+      []
+
 []
+  \R
+   []    - >right then left rotation
+  /L
 
 */
-namespace BSTNS{
+namespace BSTNS {
 namespace TreeBalancer {
 bool BalanceTheTree(NodeUPtr& root);
-namespace PrivateHelper{
-bool isLeftHeavy_(NodeUPtr& root);
-bool isRighttHeavy_(NodeUPtr& root);
-bool isBalanced_(NodeUPtr& root);
+namespace PrivateHelper {
+bool isLeftHeavy_(const Node* const node);
+bool isRightHeavy_(const Node* const node);
+bool isBalanced_(const Node* const node);
+bool isPerfectlyBalanced_(const Node* const node);
 int BalanceFactorOfNode_(const Node* const node);
-}//End PrivateHelper::
-}//End TreeBalancer::
-}//End BSTNS
+bool isLeftHeavyWithLeftHeavyChild_(const Node* const node);
+bool isLeftHeavyWithRightHeavyChild_(const Node* const node);
+bool isRightHeavyWithRightHeavyChild_(const Node* const node);
+bool isRightHeavyWithLeftHeavyChild_(const Node* const node);
+}  // End PrivateHelper::
+}  // End TreeBalancer::
+}  // End BSTNS
 
 #endif /* tree_balancer_hpp */
